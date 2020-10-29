69f746a4c1c9ad96714540c4b476add751d36a2c
Atinder-Pal <atty.pal46@gmail.com>
2020-10-29 15:02:31 -0600

    Generate C# models and a “PersonContext”;

---
+++ ExistingDBPractice/ExistingDBPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using ExistingDBPractice.Models;
+using System;
+using System.Linq;
+
+namespace ExistingDBPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Please enter a First Name: ");
+            input = Console.ReadLine().Trim();
+
+            // Dispose of whatever is in the parentheses when the code block ends.
+            using (PersonContext context = new PersonContext())
+            {
+                try
+                {                    
+                    Person person = context.Person.Where(x => x.FirstName.ToLower() == input.ToLower()).Single();              
+                    Console.WriteLine($"Full Name of this person is-{person.FirstName} {person.LastName} ");               
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR: Please ensure input is valid and try again.");
+                }
+            }
+        }
+    }
+}
